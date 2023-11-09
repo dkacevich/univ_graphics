4858b6339b9fd5a1c03960d3081dd1c6a66561b2
Dima Kacevich <d.katsevich.4@gmail.com>
2023-11-09 11:22:51 +0200

    Lab 2: Done

---
+++ image_transparency.py
@@ -0,0 +1,22 @@
+from helpers import get_user_info
+from image_tool import ImageTool
+
+
+def transparent_image():
+    source_path = ImageTool.get_source_path()
+    destination_path = ImageTool.get_destination_path()
+
+    while True:
+        try:
+            transparancy = int(get_user_info("Type transparency, it can be from 0 (completely transparent) to 255 (completely opaque)"))
+
+            if transparancy < 0 and transparancy > 255:
+                raise ValueError()
+
+            break
+        except ValueError:
+            print("You typped wrong value")
+
+
+
+    ImageTool.resize_image(source_path, destination_path, width, height)
